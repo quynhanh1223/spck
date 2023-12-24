faca08c12e5dee3ec35ca65aaa24e8dc1555041c
quynhanh1223 <nhyysspinn@gmail.com>
2023-12-24 11:58:37 +0700

    spck buoi 11

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-fetch("./data.json").then(response => {
+fetch("./data.json")
+  .then((response) => {
     return response.json();
-})
-.then((res) => {
-  // Work with JSON data here
-  console.log(res);
-})
+  })
+  .then((res) => {
+    // Work with JSON data here
+    console.log(res);
+  });
+
+fetch("./card.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((res) => {
+    const content = document.getElementById("content");
+
+    res.forEach(function (item, index) {
+      const divCard = document.createElement("div");
+      const imgTag = document.createElement("img");
+      const divBody = document.createElement("div");
+      const pTag = document.createElement("p");
+
+      divCard.appendChild(imgTag);
+      divCard.appendChild(divBody);
+      divBody.appendChild(pTag);
+
+      content.appendChild(divCard);
+      imgTag.src = item.imgUrl;
+      pTag.innerText = item.information;
+      // document.getElementById("content").classList.toggle("myStyle");
+    });
+
+    // console.log(res);
+  });
+
+  // function myFunction(){
+  //   document.getElementById("content").classList.toggle("myStyle");
+  // }
